ffd9023ef50818e497f6feeb78e1ccde2b8da7ca
8-prime <hoffmeister.stefan99@gmail.com>
2023-12-21 22:00:45 +0100

    more properties for the article. also started parsing the cool shit

--- entry.py
+++ entry.py
@@ -1,8 +1,12 @@
 import datetime
+import json
 from typing import List
 
-class NewsArticle:
-    def __init__(self, title: str = None, summary: str = None, published: datetime = None, link: str = None, id: int = None, images: List[str] = None, name: str = None):
+
+class Rental:
+    def __init__(self, title: str = None, summary: str = None, published: datetime = None, 
+                link: str = None, id: int = None, images: List[str] = None, name: str = None, 
+                cold_rent: str = None, warm_rent: str = None, area: str = None, text: str = None):
         self.title = title
         self.summary = summary
         self.published = published
@@ -10,3 +14,11 @@ class NewsArticle:
         self.id = id
         self.images = images
         self.name = name
+        self.cold_rent = cold_rent
+        self.warm_rent = warm_rent
+        self.area = area
+        self.text = text
+
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__, 
+            sort_keys=True, indent=4)
--- scape.py
+++ scape.py
@@ -1,25 +1,76 @@
 import feedparser 
-from entry import NewsArticle
+from entry import Rental
 from bs4 import BeautifulSoup
 from typing import List
 from urllib.parse import urljoin
 import requests
+import re
+import json
 
-def article_images(link): 
-    page = requests.get(link)
+
+def soup_images(soup, base_href):
+    bilder_div = soup.find("div", class_="bildergalerie-content")
+    if(not bilder_div):
+        return []
+    a_tags = bilder_div.find_all("a")
+    return [urljoin(base_href, i["href"]) for i in a_tags]
+
+def soup_cold_rent(article_text) -> str:
+    match = re.search(r"(?P<rent>\d+)€(?P<unit>kalt)", article_text)
+
+    if match:
+        return match.group("rent")
+    else:
+        return ""
+
+def soup_warm_rent(article_text) -> str:
+    match = re.search(r"(?P<rent>\d+)€(?P<unit>warm)", article_text)
+
+    if match:
+        return match.group("rent")
+    else:
+        return ""
+
+def soup_area(article_text) -> str:
+    
+    match_qm = re.search(r"(?P<square_meters>\d+)\s*qm", article_text)
+    match_m2 = re.search(r"(?P<square_meters>\d+)\s*m²", article_text)
+    match_qm2 = re.search(r"(?P<square_meters>\d+)\s*quadratmeter", article_text)
+
+    # If a match is found for qm or m², return the square meters
+    if match_qm:
+        return match_qm.group("square_meters")
+    elif match_m2:
+        return match_m2.group("square_meters")
+    elif match_qm2:
+        return match_qm2.group("square_meters")
+    return ""
+
+
+
+def soup_article(article: Rental): 
+    page = requests.get(article.link)
     if page.status_code != 200:
         return []
     
-    dom_doc = BeautifulSoup(page.content, 'html.parser')
-    bilder_div = dom_doc.find("div", class_="bildergalerie-content")
-    a_tags = bilder_div.find_all("a")
-    return [urljoin(link, i["href"]) for i in a_tags]
+    soup = BeautifulSoup(page.content, 'html.parser')
+    article.name = soup.find("p", class_="entry_name").text
+    article.text = "\n".join([x.text for x in soup.findAll("p", class_="entry_text")])
 
-def read_articles() -> List[NewsArticle]:
+    article.images = soup_images(soup, article.link)
+
+    parseable_text = article.text.lower()
+    article.cold_rent = soup_cold_rent(parseable_text)
+    article.warm_rent = soup_warm_rent(parseable_text)
+    article.area = soup_area(parseable_text)
+
+    
+
+def read_articles() -> List[Rental]:
     news_feed = feedparser.parse("https://schwarzesbrett.bremen.de/sixcms/detail.php?template=01_rss_feed_d&contentId=28555")
     articles = []
     for entry in news_feed.entries:
-        articles.append(NewsArticle(
+        articles.append(Rental(
             title = entry["title"],
             id = entry["id"],
             link = entry["link"],
@@ -33,6 +84,12 @@ if __name__ == '__main__':
     # for article in articles:
     #     article.images = article_images(article.link)
 
-    articles[0].images = article_images(articles[0].link)
-    print(articles[0].images)
+
+    for i in range(5):
+        soup_article(articles[i])
+        with open("data{num}.json".format(num = i), "w", encoding="utf-8") as file:
+            file.write(articles[i].to_json())
+
+
+
     
